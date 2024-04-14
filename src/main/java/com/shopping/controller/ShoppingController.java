9e404efb7332dd56c1ae464ecfeb5d5a20261058
imadvent <advent@duck.com>
2024-04-14 14:58:54 +0530

    implemented simple security authorization

--- src/main/java/com/shopping/controller/ShoppingController.java
+++ src/main/java/com/shopping/controller/ShoppingController.java
@@ -5,6 +5,7 @@ import com.shopping.service.ShoppingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,7 +19,8 @@ public class ShoppingController {
     @Autowired
     private ShoppingService shoppingService;
 
-    @PostMapping
+    @PostMapping(value = "/insert")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<String> create(@RequestBody ShoppingEntity shopping) {
 
         if (shopping.getShoppingId() != null && !shopping.getShoppingId().isEmpty()) {
@@ -45,42 +47,54 @@ public class ShoppingController {
         return ResponseEntity.status(HttpStatus.CREATED).body("Shopping item saved successfully");
     }
 
+    @GetMapping(value = "/check")
+    public String unsafeRequest() {
+        return "this request is not protected";
+    }
+
     @GetMapping(value = "/{shoppingId}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<?> read(@PathVariable("shoppingId") String shoppingId) {
 
         return new ResponseEntity<>(shoppingService.view(shoppingId), HttpStatus.OK);
     }
 
     @GetMapping(value = "/product")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<ShoppingEntity>> readByProductName(@RequestParam("productName") String productName) {
         return new ResponseEntity<>(shoppingService.viewByProductName(productName), HttpStatus.OK);
     }
 
     @GetMapping(value = "/custprod")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<ShoppingEntity>> readByCustomerNameOrProductName
             (@RequestParam("customerName") String customerName, @RequestParam("productName") String productName) {
         return new ResponseEntity<>(shoppingService.viewByCustomerNameOrProductName(customerName, productName), HttpStatus.OK);
     }
 
-    @GetMapping
+    @GetMapping(value = "/getall")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public List<ShoppingEntity> readAll() {
         ResponseEntity.ok();
         return shoppingService.viewAll();
     }
 
     @PutMapping(value = "/{shoppingId}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<?> update(@PathVariable("shoppingId") String shoppingId, @RequestBody ShoppingEntity shopping) {
 
         return new ResponseEntity<>(shoppingService.change(shoppingId, shopping), HttpStatus.OK);
     }
 
     @PutMapping(value = "/queryupdate")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<?> updateWithQuery(@RequestParam("shoppingId") String shoppingId, @RequestBody ShoppingEntity shopping) {
 
         return new ResponseEntity<>(shoppingService.changeByQuery(shoppingId, shopping), HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/{shoppingId}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public ResponseEntity<String> delete(@PathVariable("shoppingId") String shoppingId) {
 
         shoppingService.remove(shoppingId);
